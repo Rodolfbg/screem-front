ebc31e1ebb8e9e63ea6beda873f3555dfec6372a
Rodolfbg <rodolfo.brah@gmail.com>
2021-10-14 12:25:02 -0300

    new project

---
+++ src/tele.js
@@ -0,0 +1,11 @@
+
+function Tela(){
+   
+  return(<div id="add">
+        <input type="text" placeholder="Login"></input>
+        <input type="text" placeholder="passworld"></input>
+        <input type="button" value="Entar"></input>
+    </div>
+  )};
+
+  export default Tela; 

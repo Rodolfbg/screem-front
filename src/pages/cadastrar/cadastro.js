0c41515627c1350735cb775f6c5d39da3ba68425
Rodolfbg <rodolfo.brah@gmail.com>
2021-10-26 19:13:01 -0300

    alteraçõs do css do cadastro

--- src/pages/cadastrar/cadastro.js
+++ src/pages/cadastrar/cadastro.js
@@ -2,12 +2,12 @@ import './stil.css';
 
 function cadsastro(){
     return(
-        <div id="#cad">
+        <div id="cdd">
   
-          <div id="#cdd">
-              <input id="#ip1" type="text" placeholder="New Login"></input>
-              <input id="#ip2" type="text" placeholder="New passworld"></input>
-              <input id="#ip3" type="button" value="cadastrar"></input>
+          <div id="cad">
+              <input id="ip1" type="text" placeholder="New Login"></input>
+              <input id="ip2" type="text" placeholder="New passworld"></input>
+              <input id="ip3" type="button" value="cadastrar"></input>
           </div>
 
       </div>
